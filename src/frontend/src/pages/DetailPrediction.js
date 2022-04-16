05e141d50d6ba29d4ead9039bce8911f0cae6180
TubesForLyfe <13520160@std.stei.itb.ac.id>
2022-04-17 03:21:07 +0700

    Update detail prediction

--- src/frontend/src/pages/DetailPrediction.js
+++ src/frontend/src/pages/DetailPrediction.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Axios from 'axios'
 
 const DetailPrediction = () => {
+  const [input, setInput] = useState('');
+  const [index, setIndex] = useState(1);
+  const [predictionList, setPredictionList] = useState([]);
+
+  const getDetailPrediction = (e) => {
+    e.preventDefault();
+    Axios.post(`${process.env.REACT_APP_DNA_API}/get-detailprediction`, {
+      input: input
+    }).then((response) => {
+      setPredictionList(response.data)
+    })
+  }
+
   return (
     <div>
-      
+      <input type='text' placeholder='Input date or disease name'
+        onChange={(e) => {
+          setInput(e)
+        }}
+      />
+      <button onClick={getDetailPrediction}>Process</button>
+      <div>
+        {predictionList.map((val, key) => {
+          return (
+            <div>
+              <p>{index}. {val.TanggalPrediksi} - {val.NamaPasien} - {val.PenyakitPrediksi} - {val.TingkatKemiripan} - </p>
+              {val.Status == 1 && <p>True</p>}
+              {val.Status == 0 && <p>False</p>}
+              {setIndex(index + 1)}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
